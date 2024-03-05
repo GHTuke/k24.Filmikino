484c57f62fc9a8e4fe0efe50e25d5227b4e59025
GHTuke <tuukka.huovilainen@myy.haaga-helia.fi>
2024-03-05 10:42:08 +0200

    First setup

---
+++ src/test/java/k24/Filmikino/FilmikinoApplicationTests.java
@@ -0,0 +1,13 @@
+package k24.Filmikino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmikinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
